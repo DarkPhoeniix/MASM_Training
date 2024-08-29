c9e84266ac6aca180d9d52f2ecf312c4596ad659
DarkPhoeniix <marina.vovchuk777@gmail.com>
2024-08-29 22:26:58 +0300

    completed task 14

---
+++ Task14_JMP/Task14_JMP.asm
@@ -0,0 +1,21 @@
+INCLUDELIB kernel32.lib
+ExitProcess PROTO
+
+.DATA
+
+.CODE
+main PROC
+    XOR R14, R14
+    XOR R15, R15
+    JMP next
+    MOV R14, 100
+    next:
+    MOV R15, final
+    JMP R15
+    MOV R14, 100
+    final:
+
+CALL ExitProcess
+main ENDP
+
+END
