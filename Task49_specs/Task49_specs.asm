4f8ed132e77e56bebce08cd79277ffa2d142a7f6
DarkPhoeniix <marina.vovchuk777@gmail.com>
2024-09-14 19:45:01 +0300

    completed task 49

---
+++ Task49_specs/Task49_specs.asm
@@ -0,0 +1,17 @@
+INCLUDELIB kernel32.lib
+ExitProcess PROTO
+
+.DATA
+nums1 REAL4 44.5, 58.25, 32.6, 19.8
+nums2 REAL4 22.7, 73.2, 66.15, 12.3
+
+.CODE
+main PROC
+    MOVDQA XMM1, XMMWORD PTR [nums1]
+    MOVDQA XMM2, XMMWORD PTR [nums2]
+    MAXPS XMM1, XMM2
+
+    CALL ExitProcess
+main ENDP
+
+END
