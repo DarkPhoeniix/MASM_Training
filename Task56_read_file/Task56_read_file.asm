14c6e215637c85922f9b3044a35b631ccbcf7f97
DarkPhoeniix <marina.vovchuk777@gmail.com>
2024-09-20 21:31:33 +0300

    completed task 56

---
+++ Task56_read_file/Task56_read_file.asm
@@ -0,0 +1,74 @@
+INCLUDELIB kernel32.lib
+
+ExitProcess PROTO
+ReadFile PROTO
+GetStdHandle PROTO
+WriteConsoleA PROTO
+CreateFileA PROTO
+WriteFile PROTO
+
+GENERIC_READ            EQU 080000000h
+GENERIC_WRITE           EQU 040000000h
+FILE_SHARE_READ         EQU 1
+FILE_SHARE_WRITE        EQU 2
+OPEN_ALWAYS             EQU 4
+FILE_ATTRIBUTE_NORMAL   EQU 128
+
+clearRegisters MACRO
+    XOR RAX, RAX
+    XOR RCX, RCX
+    XOR RDX, RDX
+    XOR R8, R8
+    XOR R9, R9
+ENDM
+
+.DATA
+filePath BYTE 'Test.txt'
+fileHandle QWORD ?
+txt BYTE 'The truth is rarely pure and never simple.'
+buffer BYTE 100 DUP (?)
+num DWORD ?
+
+.CODE
+main PROC
+    clearRegisters
+    
+    SUB RSP, 64                 ; allocate shadow space for arguments
+    
+    LEA RCX, filePath
+    MOV RDX, GENERIC_READ OR GENERIC_WRITE
+    MOV R8, FILE_SHARE_READ OR FILE_SHARE_WRITE
+    MOV R9, 0
+    
+    ; pass additional arguments via stack offsets
+    MOV QWORD PTR [RSP + 32], OPEN_ALWAYS
+    MOV QWORD PTR [RSP + 40], FILE_ATTRIBUTE_NORMAL
+    MOV QWORD PTR [RSP + 48], 0
+    
+    CALL CreateFileA
+    MOV fileHandle, RAX
+    
+    clearRegisters
+    
+    MOV RCX, fileHandle         ; set parameters for ReadFile function
+    LEA RDX, buffer
+    MOV R8, LENGTHOF buffer
+    LEA R9, num
+
+    MOV QWORD PTR [RSP + 32], 0
+    CALL ReadFile
+
+    MOV RCX, -11                ; pass device code as argument 1.
+    CALL GetStdHandle           ; return handle to RAX.
+
+    MOV RCX, RAX
+    LEA RDX, buffer
+    MOV R8, LENGTHOF buffer
+    CALL WriteConsoleA
+
+    ADD RSP, 64                 ; rebalance the stack
+
+    CALL ExitProcess
+main ENDP
+
+END
