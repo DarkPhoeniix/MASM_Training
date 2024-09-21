bcfbdf2fe4fa283cc1060e5d62e6be7eaf607d20
DarkPhoeniix <marina.vovchuk777@gmail.com>
2024-09-21 20:46:28 +0300

    completed task 59

---
+++ Task59_library/Task59_library.asm
@@ -0,0 +1,30 @@
+INCLUDELIB kernel32.lib
+INCLUDELIB x64\Debug\MathF.lib
+
+ExitProcess PROTO
+
+DoAdd PROTO
+DoSub PROTO
+DoMul PROTO
+DoDiv PROTO
+
+.DATA
+
+.CODE
+
+main PROC
+    MOV RCX, 8
+    MOV RDX, 16
+    CALL DoAdd
+    
+    MOV RCX, 9
+    MOV RDX, 3
+    CALL DoSub
+    
+    CALL DoMul
+    CALL DoDiv
+
+    CALL ExitProcess
+main ENDP
+
+END
